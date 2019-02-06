ce7ee7d70b7eea1a34b3d58d7eb37ab9a03ea1a8
allan.ramirez <allan.ramirez@e2outlook.com>
2019-02-06 16:24:06 -0600

    Added Cors Origin *

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors({ origin: 'http://localhost:8080' }));
+app.use(cors({ origin: '*' }));
 
 app.use('/', indexRouter);
 app.use('/api/account', accountRouter);
