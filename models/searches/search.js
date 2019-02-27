e74bb6e32c4a076da28ac2c03a956b4806cc7460
allan.ramirez <allan.ramirez@e2outlook.com>
2019-02-27 10:26:04 -0600

    Add get My Searches

--- models/searches/search.js
+++ models/searches/search.js
@@ -65,6 +65,62 @@ exports.search = function(req) {
             data.msg.Message = "Unauthorized";
             return reject(data);
         }
-    })
+    });
+
+}
+
+exports.getMySearches = function(req) {
+    return new Promise((resolve, reject) => {
+        var data = {};
+        data.msg = { Code: 200, Message: 'Exito!', Tipo: 'n/a' };
+        var conn = config.findConfig();
+
+        const bearerHeader = req.headers['authorization'];
+        if (typeof bearerHeader !== 'undefined') {
+            const bearer = bearerHeader.split(' ');
+            const bearerToken = bearer[1];
+            req.token = bearerToken;
+            jwt.verify(req.token, 'cKWM5oINGy', (err, authData) => {
+                if (err) {
+                    data.msg.Code = 400;
+                    data.msg.Message = "Unauthorized";
+                    return reject(data);
+                } else {
+                    sql.connect(conn).then(function() {
+                        var request = new sql.Request();
+                        request.input('ProfileID', sql.Int, authData.User.Profile.id);
+
+                        request.execute("[dbo].sp_getSearches").then(function(recordsets) {
+                            let rows = recordsets.recordset;
+                            var mainKey = rows[0];
+                            var selectedKey;
+                            for (var key in mainKey) {
+                                selectedKey = key;
+                            }
+                            sql.close();
+                            return resolve(mainKey[selectedKey]);
+                        }).catch(function(err) {
+                            data.msg.Code = 500;
+                            //TODO: EN produccion cambiar mensajes a "Opps! Something ocurred."
+                            data.msg.Message = err.message;
+
+                            sql.close();
+                            return reject(data);
+                        });
+                    }).catch(function(err) {
+                        data.msg.Code = 500;
+                        data.msg.Message = err.message;
+                        sql.close();
+                        return reject(data);
+                    });
+                }
+            });
+        } else {
+            // Unauthorized
+            data.msg.Code = 400;
+            data.msg.Message = "Unauthorized";
+            return reject(data);
+        }
+    });
 
 }
--- routes/search.js
+++ routes/search.js
@@ -20,4 +20,22 @@ router.post('/search', function(req, res, next) {
         });
 });
 
+router.get('/getMySearches', function(req, res, next) {
+    searchModel
+        .getMySearches(req)
+        .then(
+            function(response) { //success
+                console.log("Success!");
+                res.send(response); //return the data
+            },
+            function(error) { //failed
+                console.error("Failed!", error);
+                res.status(404).send(error); //return error with 404
+            }
+        ).catch(function(ex) { //exception
+            console.error("Exception!", ex);
+            res.status(500).send(ex); //return exception with 500
+        });
+});
+
 module.exports = router;
